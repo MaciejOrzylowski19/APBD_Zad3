17bd1333d640938f8b775ed8e86bc34080a5800d
Maciej Orzylowski <s31569@pjwstk.edu.pl>
2025-03-21 15:30:51 +0100

    dodano GassContainer v2

--- APBD_Zad/GassContainer.cs
+++ APBD_Zad/GassContainer.cs
@@ -1,6 +1,26 @@
 ﻿namespace DeafoultNamespace;
 
-public class GassContainer
+public class GassContainer : Container, IHazard
 {
+    
+    private static int _gassCounter = 0;
+
+    public override int NextSerialNumber()
+    {
+        _gassCounter++;
+        return _gassCounter;
+    }
+
+    public override ContainerId CreateContainerID()
+    {
+        return new ContainerId(this.)
+    }
+
+    public GassContainer(int emptyMass, int height, int depth, bool hazard) : base(emptyMass, height, depth)
+    {
+        
+        
+        
+    }
     
 }
