3a7b678c20bc97ed485ba3f11ccde053bc4c3bf2
Maciej Orzylowski <s31569@pjwstk.edu.pl>
2025-03-21 14:12:13 +0100

    innitial commit

---
+++ APBD_Zad/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace DeafoultNamespace
+{
+
+
+    public class MainClass
+    {
+
+        public static void Main(string[] args)
+        {
+            
+            
+            
+        }
+        
+    }
+}
